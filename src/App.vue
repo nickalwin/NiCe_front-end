c4ae4d27c56ff52e72169941fb8d7b96b8a87987
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2024-02-13 20:40:20 +0100

    Tailwind CSS + Daisy UI configured

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">Button</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
 
 <style scoped>
