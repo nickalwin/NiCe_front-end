f90a1be334437e87d342317a931004190023d3ec
Tycjan Fortuna <242213@edu.p.lodz.pl>
2024-03-26 13:32:22 +0100

    Initial version of the question view component for the results page

---
+++ src/components/CategoryQuestionTableComponent.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="w-full flex justify-center mt-10">
+        <div class="max-w-full mx-auto bg-white rounded-xl shadow-md overflow-hidden p-8">
+            <div class="flex overflow-x-auto mb-4 space-x-4">
+                <button v-for="(group, index) in groupedQuestions" :key="index" @click="selectedGroup = group" :class="selectedGroup === group ? 'bg-blue-700' : 'bg-blue-500'" class="text-white font-bold py-2 px-4 rounded">
+                    {{ getLocalizedCategoryName(group.category) }}
+                </button>
+            </div>
+            <div v-if="selectedGroup" class="overflow-x-auto">
+                <table class="mt-4 w-full table-auto">
+                    <thead>
+                        <tr>
+                            <th class="px-4 py-2">
+                                {{ $t('fields.questions') }}
+                            </th>
+                            <th class="px-4 py-2">
+                                {{ $t('fields.answers') }}
+                            </th>
+                            <th class="px-4 py-2">
+                                {{ $t('fields.comments') }}
+                            </th>
+                            <th class="px-4 py-2">
+                                {{ $t('fields.actions') }}
+                            </th>
+                        </tr>
+                    </thead>
+                    <div>
+
+                    </div>
+                    <tbody>
+                        <tr v-for="(question, qIndex) in selectedGroup.questions.grouped_answers" :key="qIndex">
+                            <td class="border px-4 py-2">
+                                <ul>
+                                    <li>
+                                        {{ getLocalizedQuestionName(question) }}
+                                    </li>
+                                </ul>
+                            </td>
+                            <td class="border px-4 py-2 text-center">
+                                <strong class="text-blue-600">
+                                    {{ question.answer }}
+                                </strong> / 5
+                            </td>
+                            <td class="border px-4 py-2">
+                                <div v-if="question.comment == ''">
+                                    <span class="text-gray-400">
+                                        {{ $t('utils.no_comment') }}
+                                    </span>
+                                </div>
+                                <div v-else>
+                                    {{ question.comment }}
+                                </div>
+                            </td>
+                            <td class="border px-4 py-2">
+                                <button v-on:click="editScorePopup(question)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    {{ $t('utils.edit') }}
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Swal from 'sweetalert2';
+
+export default {
+    props: {
+        data: { type: Array, required: true },
+        categories: { type: Array, required: true }
+    },
+    data() {
+        return {
+            groupedQuestions: [],
+            selectedGroup: null
+        }
+    },
+    methods: {
+        getLocalizedQuestionName(questionData) {
+            var data = JSON.parse(questionData.question_data);
+
+            return data[this.$i18n.locale] ?
+                    data[this.$i18n.locale].question :
+                    data.en.question;
+        },
+        getLocalizedCategoryName(categoryData) {
+            var data = categoryData.data;
+
+            return data[this.$i18n.locale] ?
+                data[this.$i18n.locale].name :
+                data.en.name;
+        },
+        editScorePopup(question) {
+            let currentAnswer = question.answer;
+            let currentComment = question.comment;
+
+            Swal.fire({
+                title: this.$t('utils.edit_score'),
+                showCancelButton: true,
+                confirmButtonText: this.$t('utils.save'),
+                cancelButtonText: this.$t('utils.cancel'),
+                html: `
+                    <input id="swal-input1" class="swal2-input" type="range" min="1" max="5" step="1" value="${currentAnswer}">
+                    <textarea id="swal-input2" class="swal2-textarea">${currentComment}</textarea>
+                `,
+                preConfirm: () => {
+                    const score = document.getElementById('swal-input1').value;
+                    const comment = document.getElementById('swal-input2').value;
+
+                    if (!score || !comment) {
+                        Swal.showValidationMessage(this.$t('utils.required'));
+                    }
+
+                    return { score, comment };
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    const { score, comment } = result.value;
+                    // TODO: Use `score` and `comment` here
+                }
+            });
+        }
+    },
+    mounted() {
+        this.data.forEach((question) => {
+            let category = this.categories.find((category) => {
+                return category.uuid === question.category_uuid;
+            });
+
+            this.groupedQuestions.push({
+                category: category,
+                questions: question
+            });
+        });
+
+        this.selectedGroup = this.groupedQuestions[0];
+    }
+}
+</script>
