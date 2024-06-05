d97966f67648c9c5c9628195e4be83c37478dfd5
Tycjan Fortuna <242213@edu.p.lodz.pl>
2024-06-05 19:55:54 +0200

    Privacy & Cookie policy update

---
+++ src/i18n/locales/en/EN_CookiePolicy.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="privacy-policy" class="mt-4 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 class="text-4xl font-bold mb-4">Cookie Policy</h1>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">What is a cookie?</h2>
+            <p class="text-gray-700 text-base">
+                A so called ‘cookie’ is a piece of text which is being saved within your Web browser.
+                A cookie assists the website to gain information about your visit, and with these settings save settings like the set language or other settings. This will help to make your visit easier in the future.
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">Why does the platform make use of cookies?</h2>
+            <p class="text-gray-700 text-base">
+                Cookies are being used. We use it explicitly to process local storage, like the unique codes in your history.
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">Which cookies does the platform use?</h2>
+            <p class="text-gray-700 text-base">
+                Below there will be an overview of the different cookies which are being used.
+                In our privacy policy there is a description of how we secure the user with the assistance of our cookies in the manner of local storage (the UUID's of the scan).The names of the cookies can differentiate from time to time, but in general they include the following categories:
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">How can i delete the cookies?</h2>
+            <p class="text-gray-700 text-base">
+                The most browsers do have an option to turn off or delete cookies.
+                In some cases, it is even possible to block cookies for the future by changing certain settings.
+                Every browser has its own settings, for this reason they also have different functions and associated options. By choosing to turn off cookies it can prevent full usage of the platform. (For example, a part of the website could not function like it was planned).
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">In the following links of the most known browsers, you can see how to
+                turn them off:</h2>
+            <p class="text-gray-700 text-base">
+                <a href="https://help.opera.com/en/latest/web-preferences/#cookies" target="_blank"
+                    class="text-blue-500">Opera</a>
+            </p>
+            <p class="text-gray-700 text-base">
+                <a href="https://support.google.com/chrome/answer/95647?hl=nl" target="_blank"
+                    class="text-blue-500">Google Chrome</a>
+            </p>
+            <p class="text-gray-700 text-base">
+                <a href="https://support.apple.com/nl-nl/guide/deployment/welcome/web" target="_blank"
+                    class="text-blue-500">Apple Safari</a>
+            </p>
+            <p class="text-gray-700 text-base">
+                <a href="https://support.mozilla.org/nl/kb/verbeterde-bescherming-tegen-volgen-firefox-desktop?redirectslug=cookies-in-en-uitschakelen-websites-voorkeuren&redirectlocale=nl"
+                    target="_blank" class="text-blue-500">Mozilla Firefox</a>
+            </p>
+        </div>
+    </div>
+</template>
---
+++ src/i18n/locales/nl/NL_CookiePolicy.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="privacy-policy" class="mt-4 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 class="text-4xl font-bold mb-4">Cookieverklaring</h1>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">Wat is een Cookie?</h2>
+            <p class="text-gray-700 text-base">
+                Een zogeheten ‘cookie’ is een stuk tekst dat wordt opgeslagen binnen jouw webbrowser.
+                Een cookie helpt de website om informatie op te halen over jouw bezoek, en daarmee instellingen zoals de ingestelde taal of andere instellingen te houden. Dit helpt om jouw bezoek in het vervolg makkelijker te maken.
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2"> Waarom maakt het platform gebruik van cookies?</h2>
+            <p class="text-gray-700 text-base">
+                Er worden cookies gebruikt. Wij gebruiken het expliciet om lokale opslag af te handelen, zoals de unieke codes in jouw historie.
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">Welke cookies gebruikt het platform?</h2>
+            <p class="text-gray-700 text-base">
+                Hieronder is een overzicht van de verschillende cookies die worden gebruikt. In het privacybeleid is beschreven hoe we de privacy van de gebruiker beschermen mede door het gebruik van cookies in de vorm van local storage (kortom de UUID’s van de scan). De namen van de cookies zie wij gebruiken kunnen van tijd tot tijd verschillen, maar in het algemeen omsluiten ze de volgende categorieën:
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">Hoe kan ik de cookies verwijderen?</h2>
+            <p class="text-gray-700 text-base">
+                De meeste browsers bevatten een optie om cookies uit te schakelen en/of te verwijderen.
+                In sommige gevallen kan het ook voor de toekomst worden geblokkeerd door bepaalde instellingen anders in te stellen.
+                Elke browser heeft zijn eigen instellingen, en hiervoor hebben ze ook verschillende functies en bijbehorende opties. Door ervoor te kiezen deze uit te schakelen kan het voorkomen dat je niet volledig van alle opties van het platform gebruik kan maken. (Een deel van de website kan dan bijvoorbeeld niet functioneren zoals de bedoeling is).
+            </p>
+        </div>
+
+        <div class="mb-4">
+            <h2 class="font-bold text-xl mb-2">Onder de volgende links zie je van de bekendste browsers hoe je deze
+                kan uitschakelen:</h2>
+            <p class="text-gray-700 text-base">
+                <a href="https://help.opera.com/en/latest/web-preferences/#cookies" target="_blank"
+                    class="text-blue-500">Opera</a>
+            </p>
+            <p class="text-gray-700 text-base">
+                <a href="https://support.google.com/chrome/answer/95647?hl=nl" target="_blank"
+                    class="text-blue-500">Google Chrome</a>
+            </p>
+            <p class="text-gray-700 text-base">
+                <a href="https://support.apple.com/nl-nl/guide/deployment/welcome/web" target="_blank"
+                    class="text-blue-500">Apple Safari</a>
+            </p>
+            <p class="text-gray-700 text-base">
+                <a href="https://support.mozilla.org/nl/kb/verbeterde-bescherming-tegen-volgen-firefox-desktop?redirectslug=cookies-in-en-uitschakelen-websites-voorkeuren&redirectlocale=nl"
+                    target="_blank" class="text-blue-500">Mozilla Firefox</a>
+            </p>
+        </div>
+    </div>
+</template>
