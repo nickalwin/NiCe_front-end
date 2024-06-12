26e3a017887431555e21f2832d598d4be6249487
Tycjan Fortuna <242213@edu.p.lodz.pl>
2024-06-12 16:20:38 +0200

    Icon for the tooltip

--- src/components/card/QuestionCardHeader.vue
+++ src/components/card/QuestionCardHeader.vue
@@ -39,18 +39,22 @@
             </div>
         </div>
 
+
         <div class="tooltip tooltip-info tooltip-right rounded-lg px-2 py-2 text-xs mt-2"
             :data-tip="getLocalizedTooltip()">
-            <div class="flex justify-between">
-                <div v-if="question.data" class="px-5 py-2 bg-blue-400 rounded-badge">
-                    <p class="gap-2 rounded-full text-base font-medium">
-                        {{
-                            question.is_statement ?
-                            $t('fields.statement_c') :
-                            $t('fields.question_c')
+
+        <div class="flex justify-between">
+            <div v-if="question.data" class="px-5 py-2 bg-blue-400 rounded-badge">
+                <p class="gap-2 rounded-full text-base font-medium">
+                    {{
+                        question.is_statement ?
+                        $t('fields.statement_c') :
+                        $t('fields.question_c')
                         }}
 
                         <strong>{{ question.id }} / {{ totalQuestions }}</strong>
+
+                        <FontAwesomeIcon v-if="getLocalizedTooltip()" class="ml-2" icon="info" />
                     </p>
                 </div>
 
